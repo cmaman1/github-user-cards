37c6b9e0d44e1e4ddbd0e4689131ed1e892db9f2
Carolina Mamani <mamaniccarolina@gmail.com>
2020-11-09 13:18:17 -0300

    feat: javascript functions added

---
+++ index.js
@@ -0,0 +1,50 @@
+const userNameInput = document.querySelector(".username");
+const button = document.querySelector(".button");
+const imageContainer = document.querySelector(".img-wrapper");
+const userNameContainer = document.querySelector(".user-name");
+const realNameContainer = document.querySelector(".name");
+const locationContainer = document.querySelector(".location");
+const reposContainer = document.querySelector(".repos");
+const followersContainer = document.querySelector(".followers")
+const API_URL = "https://api.github.com/users/";
+
+
+const renderData = data => {
+    const profilePictureUrl = data.avatar_url;
+    const githubUserName = data.login;
+    const realName = data.name;
+    const location = data.location
+    const repos = data.public_repos;
+    const followers = data.followers;  
+
+    imageContainer.innerHTML = `<img src=${profilePictureUrl}>`;
+    userNameContainer.innerHTML = githubUserName;
+    realNameContainer.innerHTML = realName;
+    locationContainer.innerHTML = location;
+    reposContainer.innerHTML = repos;
+    followersContainer.innerHTML = followers;
+}
+
+const searchUser = () => {
+    const userName = userNameInput.value;
+
+    fetch(`${API_URL}${userName}`)
+        .then( res => {
+            res.json()
+            .then( data => {
+                renderData(data);
+            })
+        })
+        .catch( error => {
+            console.log(error)
+        })
+}
+
+const handleEnterOnInput = (event) => {
+    if (event.keyCode == 13) {
+        searchUser();
+    }
+}
+
+button.addEventListener("click", searchUser);
+userNameInput.addEventListener("keydown", handleEnterOnInput);
